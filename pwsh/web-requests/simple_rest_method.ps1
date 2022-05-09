d1c6342f0947fefe5da5afe596266e6163b0a505
Davi Marcon (@mxrcon) <davijosuemarcon@gmail.com>
2022-05-09 20:17:10 -0300

    better linting

--- pwsh/web-requests/simple_rest_method.ps1
+++ pwsh/web-requests/simple_rest_method.ps1
@@ -1,11 +1,12 @@
 $URL = 'https://globoplay.globo.com/v/10555775/'
 $html = Invoke-RestMethod $URL
-if ($html -match 'Exibição em.*-->(?<date>.*)\</span')
-{
-$date = $Matches.date
-}
+if ($html -match 'Exibição em.*-->(?<date>.*)\</span'){	
+	$date = $Matches.date
+	}
 if ($html -match 'playkit-video-info__ep-title\">(?<title>.*)<\/h1'){
-$title = $Matches.title
-}
+	$title = $Matches.title
+	}
 write-host $date
 write-host $title
+
+
